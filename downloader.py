a0530e9cca57aa1eecc9de1af06c86b90e23b2e3
Ian Davis <42723789+ianfights@users.noreply.github.com>
2025-11-23 02:03:34 -0500

    Added status information and fixed new threads not being created

--- downloader.py
+++ downloader.py
@@ -6,13 +6,15 @@ import threading
 import downloadAndParse as dp
 
 
-THREAD_COUNT = 20
+THREAD_COUNT = 100
+
 
 try:
     os.mkdir("set-images")
 except:
     # dir already exits don't create it again
     pass
+
 
 
 # To get the list of sets we will use the scryfall API, a call to just the sets page willa return 
@@ -25,7 +27,6 @@ except:
 setCodes = []
 cardAmount = []
 
-
 scryfallData = requests.get("https://api.scryfall.com/sets").json()["data"]
 
 
@@ -34,37 +35,36 @@ for mtgSet in scryfallData:
     cardAmount.append(mtgSet["card_count"])
 
 
-
-
-
-
-
 threads = []
 
 for setCode in setCodes:
     
     # print(setCodes.index(setCode))
     maxSetNum = cardAmount[setCodes.index(setCode)]
-    # print(maxSetNum)
-    try:
-        os.mkdir(f"set-images/{setCode}")
-    except:
-        # dir already exits don't create it again
-        pass
+    
     t = threading.Thread(target=dp.downloadSetImages, args=(setCode, maxSetNum))
     t.daemon = True
     threads.append(t)
     
-    
-# Start each thread
-for t in threads:
-    if threading.active_count() < THREAD_COUNT:
-        t.start()
-        print(f"Starting Thread {threading.active_count()}")
+
+progress = 0
 
 try:
     while True:
         time.sleep(0.1) # Main thread keeps running
+        if threading.active_count() < THREAD_COUNT:
+                threads[0].start()
+                threads.remove(threads[0])
+                print(f"Starting Thread {threading.active_count()}")
+                progress+=1
+        else:
+            # Print status information
+            time.sleep(15)
+            print('######################### STATUS ###################')
+            print(f'Threads running: {threading.active_count()}/{THREAD_COUNT}')
+            print(f'Sets Downloaded: {progress}/{len(setCodes)}')
+            print('####################################################')
+            
 except KeyboardInterrupt:
     print("KeyboardInterrupt detected. Exiting.")
     # Daemon threads will automatically terminate
